8439bbdd9a9da5e8dae9a94860dc799a6bc7f6ba
shikuanyu <yushikuan@outlook.com>
2020-08-05 14:49:46 +0800

    add hue and amplitude control

--- src/App.js
+++ src/App.js
@@ -5,25 +5,31 @@ import {ChromePicker} from 'react-color'
 
 
 function App() {
+  const [amplitude, setAmplitude] = useState(25);
+  const [degree, setDegree] = useState(90);
   const [hexColor, setHexColor] = useState('#006600');
   const [hues, setHues] = useState(0);
   const [beats, setBeats] = useState(0);
 
   const beatsPerMinute = beats <= 0 ? 0 : 60 / beats;
   const huesPerMinute = hues <= 0 ? 0 : 60 / hues;
+  const amplitudePercentage = 1 - (amplitude / 100);
 
   return (
     <div className="App">
       <header className="App-header">
         <div>
           <HeartIcon beatRate={beatsPerMinute} hueRate={huesPerMinute} height={"90vh"} width={"80vw"}
-                     fillColor={hexColor}/>
+                     fillColor={hexColor} amplitude={amplitudePercentage} degree={degree}/>
         </div>
         <div>
           <input type="number" placeholder="次/分钟" value={beats}
                  onChange={e => setBeats(e.target.value)}/>
           <span className='Unit'>  次/分钟 (频率) </span>
           <button onClick={() => setBeats(0)}>心跳频率清零</button>
+          <input type="range" min="1" max="100" value={amplitude}
+                 onInput={(e) => setAmplitude(e.target.value)} onChange={(e) => setAmplitude(e.target.value)}/>
+          <span className='Unit'>振幅: {amplitude}%</span>
         </div>
         <div>
           <input type="number" placeholder="次/分钟" value={hues}
@@ -31,6 +37,10 @@ function App() {
           <span className='Unit'>  次/分钟 (颜色轮转频率) </span>
           <button onClick={() => setHues(0)}>颜色轮转清零</button>
           <ChromePicker className="ColorPicker" color={hexColor} onChangeComplete={(color) => setHexColor(color.hex)}/>
+          <input type="range" min="1" max="360" value={degree}
+                 onInput={(e) => setDegree(e.target.value)} onChange={(e) => setDegree(e.target.value)}/>
+          <span className='Unit'>色域: {degree}度</span>
+
         </div>
       </header>
     </div>
--- src/HeartBeatComponents.js
+++ src/HeartBeatComponents.js
@@ -2,22 +2,38 @@ import React from "react";
 import {ReactComponent as ReactHeart} from "./heart.svg";
 import styled, {keyframes} from "styled-components";
 
-const hugRotate = keyframes`
+const hugRotate = (d) => keyframes`
   0%
   {
     filter: hue-rotate(0);
   }
-  25%
+  10%
   {
-    filter: hue-rotate(45deg);
+    filter: hue-rotate((${d}/4)deg);
+  }
+  20%
+  {
+    filter: hue-rotate((2*${d}/4)deg);
+  }
+  40%
+  {
+    filter: hue-rotate((3*${d}/4)deg);
   }
   50%
   {
-    filter: hue-rotate(90deg);
+    filter: hue-rotate(${d}deg);
   }
-  75%
+  40%
   {
-    filter: hue-rotate(45deg);
+    filter: hue-rotate((3*${d}/4)deg);
+  }
+  20%
+  {
+    filter: hue-rotate((2*${d}/4)deg);
+  }
+  10%
+  {
+    filter: hue-rotate((${d}/4)deg);
   }
   100%
   {
@@ -26,30 +42,42 @@ const hugRotate = keyframes`
 `
 ;
 
-const beatAnimation = keyframes`
+const beatAnimation = (a) => keyframes`
    0%
   {
-    transform: scale( .75 );
+    transform: scale( ${a} );
+  }
+  10%
+  {
+    transform: scale( ${a} + (1 - x)/4);
   }
   20%
   {
-    transform: scale( 0.8 );
+    transform: scale( ${a} + (2 * (1 - x))/4 );
   }
   40%
   {
-    transform: scale( .0.9 );
+    transform: scale( ${a} + (3 * (1 - x))/4 );
   }
-  60%
+  50%
   {
     transform: scale( 1 );
   }
+  60%
+  {
+    transform: scale( ${a} + (3 * (1 - x))/4 );
+  }
   80%
   {
-    transform: scale( .75 );
+    transform: scale( ${a} + (2 * (1 - x))/4 );
+  }
+  90%
+  {
+    transform: scale( ${a} + (1 - x)/4);
   }
   100%
   {
-    transform: scale( .75 );
+    transform: scale( ${a} );
   }
 `;
 
@@ -59,9 +87,10 @@ const StyleLogo = styled(ReactHeart)`
   fill: ${props => props.fillColor};
   display: block;
   margin: auto;
-  animation: ${beatAnimation} infinite ${props => props.beatRate}s linear, ${hugRotate} infinite ${props => props.hueRate}s linear; 
+  animation: ${props => beatAnimation(props.amplitude)} infinite ${props => props.beatRate}s linear, ${props => hugRotate(props.degree)} infinite ${props => props.hueRate}s linear; 
 `;
 
-export const HeartIcon = ({beatRate, hueRate, width, height, fillColor}) => (
-  <StyleLogo beatRate={beatRate} hueRate={hueRate} width={width} height={height} fillColor={fillColor}/>
+export const HeartIcon = ({beatRate, hueRate, width, height, fillColor, amplitude, degree}) => (
+  <StyleLogo beatRate={beatRate} hueRate={hueRate} width={width} height={height} fillColor={fillColor}
+             amplitude={amplitude} degree={degree}/>
 )
